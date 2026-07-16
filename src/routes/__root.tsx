b57287ea7600d42d2abb95152e05a38cf8513b80
Harshal Thakur <official.harshalthakur@gmail.com>
2026-07-16 15:40:58 +0530

    no scroll

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -143,12 +143,9 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="flex min-h-svh flex-col bg-background">
-        
-        {/* 3. Conditionally render the header: only show if NOT on a dashboard route */}
+      <div className="flex h-svh flex-col bg-background overflow-hidden">
         {!isDashboardRoute && <SiteHeader />}
-        
-        <main id="main" className="flex-1 flex flex-col">
+        <main id="main" className="flex-1 min-h-0 flex flex-col">
           <Outlet />
         </main>
         <SiteFooter />
